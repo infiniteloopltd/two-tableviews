95bbc372fdada0ed3a669bbfccd7297bf8430f6a
Fiach Reid <fiach.reid@gmail.com>
2018-04-23 12:33:38 +0100

    added code

--- experiments/ViewController.swift
+++ experiments/ViewController.swift
@@ -8,11 +8,77 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate  {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        var count:Int?
+        
+        if tableView == self.tableView1 {
+            count = sampleData.count
+        }
+        
+        if tableView == self.tableView2 {
+            count =  sampleData1.count
+        }
+        
+        return count!
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell:UITableViewCell?
+        
+        if tableView == self.tableView1 {
+            cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+            let previewDetail = sampleData[indexPath.row]
+            cell!.textLabel!.text = previewDetail.title
+            
+        }
+        
+        if tableView == self.tableView2 {
+            cell = tableView.dequeueReusableCell(withIdentifier: "cell1", for: indexPath)
+            let previewDetail = sampleData1[indexPath.row]
+            cell!.textLabel!.text = previewDetail.title
+            
+        }
+        
+        
+        
+        return cell!
+    }
+    
 
+    @IBOutlet weak var tableView1: UITableView!
+    
+    @IBOutlet weak var tableView2: UITableView!
+    
+    struct PreviewDetail {
+        let title: String
+        let preferredHeight: Double
+    }
+    
+    let sampleData = [
+        PreviewDetail(title: "Small", preferredHeight: 160.0),
+        PreviewDetail(title: "Medium", preferredHeight: 320.0),
+        PreviewDetail(title: "Large", preferredHeight: 0.0) // 0.0 to get the default height.
+    ]
+    
+    let sampleData1 = [
+        PreviewDetail(title: "One", preferredHeight: 160.0),
+        PreviewDetail(title: "Two", preferredHeight: 320.0),
+        PreviewDetail(title: "Three", preferredHeight: 0.0), // 0.0 to get the default height.
+        PreviewDetail(title: "More", preferredHeight: 0.0) // 0.0 to get the default height.
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableView1.dataSource = self
+        tableView1.delegate = self
+        tableView1.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        tableView2.dataSource = self
+        tableView2.delegate = self
+        tableView2.register(UITableViewCell.self, forCellReuseIdentifier: "cell1")
     }
 
     override func didReceiveMemoryWarning() {
